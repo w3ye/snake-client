e89b44f5a75682093d6e49fbdb5765b3f8ecd2d4
William Ye <w3ye@pm.me>
2021-07-28 13:21:13 -0400

    client.js added. Connection function moved from play.js to client.js

---
+++ client.js
@@ -0,0 +1,18 @@
+const net = require('net');
+
+const connect = () => {
+  const conn = net.createConnection({
+    host: '135.23.223.133',
+    port: 50542,
+  });
+
+  conn.setEncoding('utf8');
+  conn.on('connect', () => {
+    console.log("connected");
+  });
+  conn.on('data', (data) => {
+    console.log(data);
+  });
+};
+
+module.exports = {connect};
