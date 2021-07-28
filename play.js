f98243ae4338beec362988a3a5624457c199cf8e
William Ye <w3ye@pm.me>
2021-07-28 13:21:55 -0400

    Import connect from client.js

--- play.js
+++ play.js
@@ -1,19 +1,6 @@
 const net = require('net');
-
-const connect = () => {
-  const conn = net.createConnection({
-    host: '135.23.223.133',
-    port: 50542,
-  });
-
-  conn.setEncoding('utf8');
-  conn.on('connect', () => {
-    console.log("connected");
-  });
-  conn.on('data', (data) => {
-    console.log(data);
-  });
-};
+const client = require('./client');
+const connect = client.connect;
 
 console.log("Connecting ... ");
 connect();
